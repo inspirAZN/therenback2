d7da82354a318a62c7b54956b40b07b7aedc2e4f
inspirAZN <jcaluza@ucsd.edu>
2014-02-25 22:34:11 -0800

    asdkjfadskljf

--- public/js/initializeMoments.js
+++ public/js/initializeMoments.js
@@ -43,27 +43,9 @@ function initializePage() {
 		// issue the GET request
 		// $.get(url_call, changeIcon);
 		$.get(url_call, function() {
-			window.location.href = '/'; //reload the page
+			window.location.href = '/moments'; //reload the page
 		});
 
-
-	});
-
-	$('#newProjectSubmitButton').click(function(e) {
-		console.log('clicked');
-		var title = $('#new-project-form #title').val();
-		var image_url = $('#new-project-form #image_url').val();
-		var date = $('#new-project-form #date').val();
-		var summary = $('#new-project-form #summary').val();
-		var json = {
-			'project_title': title,
-			'image_url': image_url,
-			'date':  date,
-			'summary': summary
-		};
-		$.post('/project/new', json, function() {
-			window.location.href = '/'; // reload the page
-		});
 	});
 }
 
